bd82780edf8e0236d6734447f87451a591051b98
Aksha <71023066+Uniyo-Ltd@users.noreply.github.com>
2023-11-30 13:51:06 +0200

    Create stripe_endpoint.py

---
+++ stripe_endpoint.py
@@ -0,0 +1,37 @@
+import stripe
+from flask import Flask, request, jsonify
+
+export STRIPE_SECRET_KEY="pk_live_51HpeESHLc8y1CsAnFfFkXR4DcT0aYH7qFwuWO0J4Z0aHhSs4A7t4jbhdG7k4yRdsbfSWuJW8AdiJcJF5JToFTnBd00ELeJ23Fc"
+
+app = Flask(__name__)
+
+@app.route("/create-payment-intent", methods=["POST"])
+def create_payment_intent():
+    # Extract payment data from the request
+    amount = request.json["amount"]
+    currency = request.json["currency"]
+
+    # Create a payment intent
+    stripe.api_key = STRIPE_SECRET_KEY
+    payment_intent = stripe.PaymentIntent.create(
+        amount=amount,
+        currency=currency,
+    )
+
+    # Return the payment intent ID to the Flutter app
+    return jsonify({"clientSecret": payment_intent["client_secret"]})
+
+@app.route("/confirm-payment-intent", methods=["POST"])
+def confirm_payment_intent():
+    # Extract payment intent ID from the request
+    payment_intent_id = request.json["paymentIntentId"]
+
+    # Confirm the payment intent
+    stripe.api_key = STRIPE_SECRET_KEY
+    payment_intent = stripe.PaymentIntent.confirm(payment_intent_id)
+
+    # Return the payment status to the Flutter app
+    return jsonify({"status": payment_intent["status"]})
+
+if __name__ == "__main__":
+    app.run(debug=True)
